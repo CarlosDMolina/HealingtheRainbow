599540ed6ea2ebe5e4ce3a96140a9fd31722acae
CarlosDMolina <cakimolina86@gmail.com>
2022-11-29 13:36:12 -0600

    shop section

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -1,9 +1,30 @@
-import React from "react";
- 
-export default function Shop() {
- return (
-   <div style={{display: 'flex', justifyContent: 'center'}}>
-     <h1>Shop</h1>
-   </div>
- );
+import React from 'react';
+import styled from 'styled-components'
+
+function Shop() {
+  return (
+    <>
+      <Section>
+
+      </Section>
+    </>
+  )
 }
+
+export default Shop;
+
+const Section = styled.section`
+  position: relative;
+  height: 100vh;
+  width: 100vw;
+  overflow: hidden;
+  background-color: ${props => props.dark ? 'var(--black)' : 'var(--tan)'};
+
+  ${props => props.dark ? `` : `@media (max-width: 992px ) and (orientation: landscape) {
+  padding-top: 20vh;
+}
+
+@media (max-width: 479px) {
+  padding-top: 0vh;
+}`};
+`
